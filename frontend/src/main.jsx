145421cd1f67a1a43eed494aae26f2894882e6a2
Johana Sevillano <johanasev@gmail.com>
2025-09-24 00:31:57 -0500

    Se actualizan archivos para aplicar estilos en página de inicio

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx"; // <-- Asegúrate de que esta ruta sea correcta
+import "./app/index.css"; // <-- La ruta al CSS global
+import "./app/App.css"; // <-- Si tienes un segundo archivo CSS, esta también debe ser la ruta correcta
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
