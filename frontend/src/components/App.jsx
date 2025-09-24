fde5885e7130fe71e1cac22b8b30ed4947dab2e7
Johana Sevillano <johanasev@gmail.com>
2025-09-24 00:19:39 -0500

    Se agrega la pantalla inicial con estilos de tailwnd aplicados

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,11 @@
+// src/components/App.jsx
+
+import React from 'react';
+import LoginPage from '../app/login/page.jsx'; // Correcto
+
+function App() {
+  return (
+    <LoginPage />
+  );
+}
+export default App;
